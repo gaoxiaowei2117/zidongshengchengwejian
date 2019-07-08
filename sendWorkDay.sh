41d0b7cc313edfc02b464c98e940a58d469af73f
gaoxiaowei2117 <gaoxiaowei2117@gmail.com>
2019-07-08 16:09:01 +0800

    version 4.7.2 change haitang.

--- sendWorkDay.sh
+++ sendWorkDay.sh
@@ -7,5 +7,7 @@ echo "${time}" > $LOG_FILE
 cd $WORK_PATH
 ./auto.sh ./changhe/shimoban/ ./changhe/1 >> $LOG_FILE
 ./auto.sh ./haitang/ ./haitang/1 >> $LOG_FILE
+./auto.sh ./haitang/ ./haitang/1 >> $LOG_FILE
 ./auto.sh ./kangda/jiaquanjiance/ ./kangda/jiaquanjiance/2 >> $LOG_FILE
 ./auto.sh ./yishang/ yishang/1/ >> $LOG_FILE
+./auto.sh aishangjia/ aishangjia/1 >> $LOG_FILE
