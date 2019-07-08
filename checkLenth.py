a710b76920549753fb3cffbd15929fe926444619
root <gaoxw2117@vip.163.com>
2019-07-08 08:21:58 +0000

    version 4.7.1 The phone number of haitang was removed.

---
+++ checkLenth.py
@@ -0,0 +1,38 @@
+# -*- coding:utf8 -*-
+import json
+import sys
+import os
+import re
+import random
+import codecs
+import linecache
+
+sys.stdout=codecs.getwriter('utf8')(sys.stdout.detach())
+
+
+def CheckFile(phasesList, fileName):
+    fileWriter=open(fileName,'w',encoding='utf8')
+    for paraIndex in range(len(phasesList) - 1) :
+        print(paraIndex)
+        print(phasesList[paraIndex])
+        if len(phasesList[paraIndex])<=150:
+            #fileWriter.write(paraIndex, len(phasesList[paraIndex]), phasesList[paraIndex]) 
+            fileWriter.write(str(paraIndex+1)+',') 
+            fileWriter.write(str(len(phasesList[paraIndex]))+',') 
+            fileWriter.write(phasesList[paraIndex]) 
+            fileWriter.write('\n') 
+    fileWriter.close()
+
+if __name__ == "__main__": 
+    if len(sys.argv) < 2:
+        print('python3 server.py {directory}')
+        print('eg: python3 server.py ./')
+        sys.exit()
+    #p=re.compile(\n',re.S);
+    p=re.compile('\n',re.S);
+    f= open(sys.argv[1],'r',encoding='utf8')
+    fileContent = f.read()
+    phasesList=p.split(fileContent)
+    f.close()
+    fileName='files/1.txt'
+    CheckFile(phasesList, fileName)
